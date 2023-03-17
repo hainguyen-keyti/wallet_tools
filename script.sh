847443aee2dc1115519dd7e904d636c3c7bcfbc6
hainguyen-keyti <nhonnh9939@gmail.com>
2023-03-17 10:58:45 +0700

    update

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@
 isLoop=true
 while $isLoop
 do
+  echo ""
+  echo "--------------- / SELECT / ---------------"
   echo "Please select the function you need below:"
   echo "1. Generate a random mnemonic"
   echo "2. Get the xprv and xpub information from the mnemonic"
@@ -13,16 +15,23 @@ do
   read choice
 
   if [ $choice -eq 1 ]; then
+    echo "--------------- / RESULT / ---------------"
+    echo ""
     node randomMnemonic.js
   elif [ $choice -eq 2 ]; then
     echo "Please enter your mnemonic:"
     read mnemonic
+    echo "--------------- / RESULT / ---------------"
+    echo ""
     node getKeyPairsByMnemonic.js ${mnemonic}
   elif [ $choice -eq 3 ]; then
     echo "Please enter your xpub:"
     read xpub
     echo "Please enter hdPath:"
+    echo "(In Wallet service format hdPath is: \"m/0/0/1\", \"m/0/0/2\", \"m/0/1/1\")"
     read hdPath
+    echo "--------------- / RESULT / ---------------"
+    echo ""
     node getAddressByPath.js ${xpub} ${hdPath}
   elif [ $choice -eq 4 ]; then
     echo "Please enter the \"CONTAINER ID\" of p2pay_wallet_1:"
@@ -34,6 +43,8 @@ do
     read xpub
     echo "Please enter your xprv:"
     read xprv
+    echo "--------------- / RESULT / ---------------"
+    echo ""
     docker exec -ti ${containerId} curl --location \'http://localhost:3000/add_wallet\' --header \'Content-Type: application/json\' --data \'{\"service\":\"${service}\", \"xpub\":\"${xpub}\", \"hotWallet\":\"\", \"xpriv\":\"${xprv}\", \"coldWallet\":\"\", \"mnemonic\":\"\"}\'
   elif [ $choice -eq 5 ]; then
     isLoop=false
